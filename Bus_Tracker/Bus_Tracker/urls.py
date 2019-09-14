107eb8b55e721e673b947d477df206151d960c68
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2019-09-14 16:41:31 -0500

    Making the map viewer the homepage..? Still figuring things out.

--- Bus_Tracker/Bus_Tracker/urls.py
+++ Bus_Tracker/Bus_Tracker/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('mapviewer/', include('mapviewer.urls')),
+    path('', include('mapviewer.urls')),
+    # path('mapviewer/', include('mapviewer.urls')),
     path('admin/', admin.site.urls),
 ]
