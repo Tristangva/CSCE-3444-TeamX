9bcbc0bb3e6d2c895f3b8642771897a65ac72928
Tristan White <tristanwhite.gva@gmail.com>
2019-09-16 11:10:09 -0500

    Added models for Bus and Routes. Made the admin site able to modify them

--- Bus_Tracker/mapviewer/models.py
+++ Bus_Tracker/mapviewer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Bus(models.Model):
+    id = models.CharField(primary_key=True, max_length=30)
+    route = models.CharField(max_length=30)
+    capacity = models.IntegerField()
+
+
+class Routes(models.Model):
+    route_id = models.CharField(primary_key=True, max_length=30)  # route id = key
+    name = models.CharField(max_length=30)  # name of route
+    color = models.CharField(max_length=30)  # color
+    # Days of week
+    # time array
