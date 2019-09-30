347c6bd2ab1d94c07dc594f4e71f85304fbd89ee
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2019-09-30 11:13:03 -0500

    Routes Sidebar sort of created

--- Bus_Tracker/frontend/src/components/App.js
+++ Bus_Tracker/frontend/src/components/App.js
@@ -1,35 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import RoutesSidebar from "./RoutesSidebar"
 import TestButton from "./TestButton.js"
-import './App.css';
 // import DataProvider from "./DataProvider";
 // import Table from "./Table";
 
 
 const App = () => (
-    <div className="is-warning">
-        <div>
-            <h1> Hello World </h1>
-        </div>
-        <TestButton/>
-        <section className="hero is-dark">
-            <div className="hero-body">
-                <div className="container">
-                    <h1 className="title">
-                        Jon made all of this with React
-                    </h1>
-                    <h2 className="subtitle">
-                        With the help of styling from Bulma
-                    </h2>
-                </div>
-            </div>
-        </section>
-        <section className="hero is-link is-fullheight is-fullwidth">
-            <div className="hero-body is-danger has-text-centered">
-                <h1>Test</h1>
-            </div>
-        </section>
+    <div>
+        <RoutesSidebar/>
     </div>
 );
-const wrapper = document.getElementById("app");
-wrapper ? ReactDOM.render(<App/>, wrapper) : null;
+
+export default App;
---
+++ Bus_Tracker/frontend/src/components/RoutesSidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RoutesSidebar extends React.Component {
+    render(){
+        return (
+            <button id="RoutesButton" className="RoundButton">Test</button>
+            <div id="RoutesSidebar">
+                <h3>List Of Routes</h3>
+            </div>
+        );
+    }
+}
+
+export default RoutesSidebar;
