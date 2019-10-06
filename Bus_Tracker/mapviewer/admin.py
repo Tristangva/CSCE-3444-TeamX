bd9b25e5e3024d22c3a808debca3b498398827fd
Tristan White <tristanwhite.gva@gmail.com>
2019-10-06 15:08:15 -0500

    Added a map model. Displays map on admin site, but can't make a default location.

--- Bus_Tracker/mapviewer/admin.py
+++ Bus_Tracker/mapviewer/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Bus, Routes
+from .models import Bus, Routes, map
+from django.contrib.gis.admin import OSMGeoAdmin
 
 # Register your models here.
 admin.site.register(Bus)
-admin.site.register(Routes)
+admin.site.register(Routes),
+
+admin.site.register(map)
+class mapAdmin(OSMGeoAdmin):
+
+    default_lon = 33.215079
+    default_lat = -97.133103
+    default_zoom = 15
+    readonly_fields = ('Latitude', 'Longitude')
+
--- Bus_Tracker/mapviewer/models.py
+++ Bus_Tracker/mapviewer/models.py
@@ -1,13 +1,21 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Map module?
+# Map module
+class map(models.Model):
+    city = models.CharField(max_length=50, default='Denton, Tx') #city name
+    location = models.PointField()
 
-# class for bus
+    # class for bus
 class Bus(models.Model):
     id = models.CharField(primary_key=True, max_length=30)  # key
     route = models.CharField(max_length=30)
     capacity = models.IntegerField()
-    status = models.CharField(max_length=30, default='In service')  # maintenance status
+    STATUS_CHOICES = [ # different choices for bus status
+        ('In service', 'In service'),
+        ('Maintenance', 'Maintenance'),
+    ]
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='In service')  # maintenance status
+    # if it is being used right now
 
 # Class for the routes
 class Routes(models.Model):
@@ -17,3 +25,9 @@ class Routes(models.Model):
     # Days of week
     # time array
     # stops on route
+
+# For some reason migrations mess up with this. Will fix once I got the map centered.
+"""class stops(models.Model):
+    stop_id = models.IntegerField() #stop id
+    stop_name = models.CharField(max_length=50) # stop name
+    location = models.PointField() #location"""
