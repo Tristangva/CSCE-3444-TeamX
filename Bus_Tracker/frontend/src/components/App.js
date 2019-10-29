bc7715ed7ae670af76c658f8809b4ddf56941a94
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2019-10-29 14:03:34 -0500

    Converted the App to a class rather than const

--- Bus_Tracker/frontend/src/components/App.js
+++ Bus_Tracker/frontend/src/components/App.js
@@ -3,16 +3,26 @@ import RoutesSidebar from "./RoutesSidebar"
 import TestButton from "./TestButton.js"
 // import DataProvider from "./DataProvider";
 // import Table from "./Table";
-import MapMarkerController from "./MapMarkerController"
+import {initMap} from "./MapMarkerController"
 
-const App = () => (
-    <div>
-        <RoutesSidebar/>
-        {/*<Table/>*/}
-        <iframe id="map"
-                src="https://www.google.com/maps/embed/v1/view?zoom=14&center=33.2075%2C-97.1526&key=AIzaSyDgYMarUznWd6jqCkxDh0GBnfnZ2DGsp0k"
-                />
-    </div>
-);
+class App extends React.Component {
+
+    render(){
+
+        return(
+            <div>
+                <RoutesSidebar/>
+                {/*<Table/>*/}
+                <iframe id="map"
+                        src="https://www.google.com/maps/embed/v1/view?zoom=14&center=33.2075%2C-97.1526&key=AIzaSyDgYMarUznWd6jqCkxDh0GBnfnZ2DGsp0k"
+                        />
+                {/*<div id="map"></div>*/}
+                {/*<script async defer*/}
+                {/*        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCkUOdZ5y7hMm0yrcCQoCvLwzdM6M8s5qk&callback=initMap">*/}
+                {/*</script>*/}
+            </div>
+        )
+    }
+}
 
 export default App;
