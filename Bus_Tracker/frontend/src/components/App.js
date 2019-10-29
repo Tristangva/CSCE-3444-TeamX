41cfef2150ac8db03f3a73b2dc25e9a317d11d7f
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2019-10-29 13:17:11 -0500

    google maps working with api

--- Bus_Tracker/frontend/src/components/App.js
+++ Bus_Tracker/frontend/src/components/App.js
@@ -3,11 +3,15 @@ import RoutesSidebar from "./RoutesSidebar"
 import TestButton from "./TestButton.js"
 // import DataProvider from "./DataProvider";
 // import Table from "./Table";
-
+import MapMarkerController from "./MapMarkerController"
 
 const App = () => (
     <div>
         <RoutesSidebar/>
+        {/*<Table/>*/}
+        <iframe id="map"
+                src="https://www.google.com/maps/embed/v1/view?zoom=14&center=33.2075%2C-97.1526&key=AIzaSyDgYMarUznWd6jqCkxDh0GBnfnZ2DGsp0k"
+                />
     </div>
 );
 
