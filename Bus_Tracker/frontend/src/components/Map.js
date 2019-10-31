83ba168058699b37035df2b25e16ef494a47cd76
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2019-10-30 19:31:22 -0500

    Converted Map to a class rather than const

--- Bus_Tracker/frontend/src/components/Map.js
+++ Bus_Tracker/frontend/src/components/Map.js
@@ -2,25 +2,32 @@ import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 
-const Map = (props) => {
-    const [center, setCenter] = useState({lat: 33.219348, lng: -97.146405 });
-    const [zoom, setZoom] = useState(11);
-    return (
-        <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyDgYMarUznWd6jqCkxDh0GBnfnZ2DGsp0k' }}
-          defaultCenter={center}
-          defaultZoom={zoom}
-        >
-          <Marker
-            lat={33.219348}
-            lng={-97.146405}
-            name="My Marker"
-            color="blue"
-          />
-        </GoogleMapReact>
-      </div>
-    );
+class Map extends React.Component {
+    static defaultProps = {
+    center: {
+      lat: 33.219348,
+      lng: -97.146405
+    },
+    zoom: 16
+  };
+    render(){
+        return (
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                  bootstrapURLKeys={{ key: 'AIzaSyDgYMarUznWd6jqCkxDh0GBnfnZ2DGsp0k' }}
+                  defaultCenter={this.props.center}
+                  defaultZoom={this.props.zoom}
+                >
+                    <Marker id="testMarker"
+                        lat={33.219348}
+                        lng={-97.146405}
+                        name="My Marker"
+                        color="blue"
+                    />
+                </GoogleMapReact>
+          </div>
+        );
+    }
 }
 
 export default Map;
