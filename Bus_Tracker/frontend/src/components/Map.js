81fc2542e3ed87dadb2d0ebf2a7677565ad7b210
Tristan White <tristanwhite.gva@gmail.com>
2019-11-03 13:31:11 -0600

    Dot is centered on courthouse now

--- Bus_Tracker/frontend/src/components/Map.js
+++ Bus_Tracker/frontend/src/components/Map.js
@@ -3,7 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 
 const Map = (props) => {
-    const [center, setCenter] = useState({lat: 33.219348, lng: -97.146405 });
+    const [center, setCenter] = useState({lat: 33.215079, lng: -97.133103 });
     const [zoom, setZoom] = useState(11);
     return (
         <div style={{ height: '100vh', width: '100%' }}>
@@ -13,8 +13,8 @@ const Map = (props) => {
           defaultZoom={zoom}
         >
           <Marker
-            lat={33.219348}
-            lng={-97.146405}
+            lat={33.215079}
+            lng={-97.133103}
             name="My Marker"
             color="blue"
           />
