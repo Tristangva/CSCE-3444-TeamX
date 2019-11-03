da5b1f2d36893848ec9ca7cd28b18fc423afe41b
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2019-11-03 13:04:25 -0600

    Updated code for the branch

--- Bus_Tracker/frontend/src/components/Map.js
+++ Bus_Tracker/frontend/src/components/Map.js
@@ -3,6 +3,16 @@ import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 
 class Map extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          location: [
+              {lat: 32.219348, lng: -97.146405}
+          ]
+        };
+
+        this.UpdateMarkers = this.UpdateMarkers.bind(this)
+  }
     static defaultProps = {
     center: {
       lat: 33.219348,
@@ -10,10 +20,32 @@ class Map extends React.Component {
     },
     zoom: 16
   };
+
+    UpdateMarkers() {
+
+    }
+
+    componentDidMount() {
+      this.interval = setInterval(() => this.UpdateMarkers(), 1000);
+    }
+    componentWillUnmount() {
+      clearInterval(this.interval);
+    }
     render(){
+        {
+            var second = Math.floor(Date.now()/1000)
+            console.log(second)
+            if(second % 2 === 0){
+                console.log("test 1!")
+                var children = document.body.childNodes
+            }
+            else {
+                console.log("test 2!")
+            }
+        }
         return (
             <div style={{ height: '100vh', width: '100%' }}>
-                <GoogleMapReact
+                <GoogleMapReact classname="map"
                   bootstrapURLKeys={{ key: 'AIzaSyDgYMarUznWd6jqCkxDh0GBnfnZ2DGsp0k' }}
                   defaultCenter={this.props.center}
                   defaultZoom={this.props.zoom}
