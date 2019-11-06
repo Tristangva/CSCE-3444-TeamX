c915f77f0f87545f1caba4e5334aba67466964e3
Jon Kleehammer <Jon.Kleehammer@gmail.com>
2019-11-05 21:14:05 -0600

    Discovery park bus stop markers added to the map

--- Bus_Tracker/frontend/src/components/Map.js
+++ Bus_Tracker/frontend/src/components/Map.js
@@ -24,28 +24,27 @@ class Map extends React.Component {
     UpdateMarkers() {
 
     }
-
-    componentDidMount() {
-      this.interval = setInterval(() => this.UpdateMarkers(), 1000);
-    }
-    componentWillUnmount() {
-      clearInterval(this.interval);
-    }
     render(){
-        {
-            var second = Math.floor(Date.now()/1000)
-            console.log(second)
-            if(second % 2 === 0){
-                console.log("test 1!")
-                var children = document.body.childNodes
-            }
-            else {
-                console.log("test 2!")
-            }
-        }
-        const data =[   {"name":"test1", "color":"red", "lat":33.209348, "lng": -97.136405},
-                        {"name":"test2", "color":"blue", "lat":33.229348, "lng": -97.156405},
-                        {"name":"test2", "color":"green", "lat":33.219349, "lng": -97.146406}];
+        const data =[   {"name": "2384098", "color":"green", "lat": 33.2147452, "lng":-97.1579706},
+                        {"name": "2394646", "color":"green", "lat": 33.211193, "lng":-97.146198},
+                        {"name": "838961", "color":"green", "lat": 33.215695, "lng":-97.139411},
+                        {"name": "838962", "color":"green", "lat": 33.21570111, "lng":-97.14362844},
+                        {"name": "838963", "color":"green", "lat": 33.215747, "lng":-97.146264},
+                        {"name": "838964", "color":"green", "lat": 33.21573087, "lng":-97.14945763},
+                        {"name": "838965", "color":"green", "lat": 33.21576901, "lng":-97.15388529},
+                        {"name": "838966", "color":"green", "lat": 33.21579013, "lng":-97.15727745},
+                        {"name": "838967", "color":"green", "lat": 33.2158306, "lng":-97.1596222},
+                        {"name": "838993", "color":"green", "lat": 33.213498, "lng":-97.144214},
+                        {"name": "839017", "color":"green", "lat": 33.24457836, "lng":-97.14051087},
+                        {"name": "839093", "color":"green", "lat": 33.211442, "lng":-97.144268},
+                        {"name": "839365", "color":"green", "lat": 33.211468833204, "lng":-97.15359322609},
+                        {"name": "839366", "color":"green", "lat": 33.213668803, "lng":-97.151442641},
+                        {"name": "839367", "color":"green", "lat": 33.213998, "lng":-97.148406},
+                        {"name": "839371", "color":"green", "lat": 33.214719803, "lng":-97.159470643},
+                        {"name": "839372", "color":"green", "lat": 33.213860803, "lng":-97.155108642},
+                        {"name": "839373", "color":"green", "lat": 33.211860803, "lng":-97.155152642},
+                        {"name": "839387", "color":"green", "lat": 33.253467, "lng":-97.153912},
+                        {"name": "839397", "color":"green", "lat": 33.210993729, "lng":-97.146209593}];
 
         const stops = data.map((d) => <Marker color={d.color} key={d.name} lat={d.lat} lng={d.lng}>{d.name}</Marker>);
 
